4229538eb4a203b28a67c968b57de6111f3a6045
xxlamedaxx <leonardo.lameda@sou.inteli.edu.br>
2025-05-09 13:16:31 -0300

    adiciona tudo quase

--- server.js
+++ server.js
@@ -3,22 +3,6 @@ const express = require("express");
 const app = express();
 const db = require("./config/db");
 const path = require("path");
-const PORT = 3000;
-
-// Middleware para processar JSON
-app.use(express.json());
-
-// Rotas
-const routes = require("./routes/index");
-app.use("/", routes);
-
-// Inicializa o servidor
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 
 db.connect()
   .then(() => {
@@ -26,21 +10,13 @@ db.connect()
 
     app.use(express.json());
 
-    const userRoutes = require("./routes/userRoutes");
-    app.use("/users", userRoutes);
-
-    const frontendRoutes = require("./routes/frontRoutes");
-    app.use("/", frontendRoutes);
-
-    // Middleware para lidar com erros de rota não encontrada
-    app.use((req, res, next) => {
-      res.status(404).send("Página não encontrada");
-    });
-
-    // Middleware para lidar com erros internos do servidor
-    app.use((err, req, res, next) => {
-      console.error(err.stack);
-      res.status(500).send("Erro no servidor");
+    app.get("/", async (req, res) => {
+      try {
+        const result = await db.query("SELECT NOW()");
+        res.send(`Hora atual no banco: ${result.rows[0].now}`);
+      } catch (err) {
+        res.status(500).send("Erro ao conectar com o banco.");
+      }
     });
 
     const PORT = process.env.PORT || 3000;
